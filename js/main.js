de5e88dd4f039dbaaf65ef7122cecee754470a69
John N. Norman <normanj777@gmail.com>
2020-08-27 14:59:29 -0400

    add switch animation

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ const getTrendColor = (num) => {
     }
 }
 
-
+// set caret: up (positive), down (negative)
 const setCarrot = (num, carrot) => {
     elem = document.getElementById(carrot)
     if (num > 0) {
@@ -262,6 +262,24 @@ const dsiplayYoutube = () => {
     setCarrot(trendTertiary,  tertiaryCaret)
     
 }
+
+// switch dark mode and light mode
+let switched = false
+const switching = () => {
+    const theSwitch = document.getElementById('switch')
+    if (switched) {
+        theSwitch.classList.add('switched')
+        switched = false
+    } else {
+        theSwitch.classList.remove('switched')
+        switched = true
+    }
+}
+
+document.getElementById('switch').addEventListener( 'click', (e) => {
+    e.preventDefault()
+    switching()
+})
 
 setTotalFollowers();
 displayFacebook();
